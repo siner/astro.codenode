c8748bfc83c3eb904ec7d4b0b690622910d7e8e5
Fran Moreno <fran.siner@gmail.com>
2025-02-08 15:19:47 +0100

    Removing cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 import react from '@astrojs/react'
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
-  adapter: cloudflare()
+  integrations: [react()]
 })
